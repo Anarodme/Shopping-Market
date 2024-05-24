a21c365e54ad49966fdc382830046b2c33eabf6b
Anarodme <109930993+Anarodme@users.noreply.github.com>
2024-05-24 12:02:03 -0600

    Add media queries

--- src/Component/TopSell/TopSell.js
+++ src/Component/TopSell/TopSell.js
@@ -3,23 +3,21 @@ import "./TopSell.css";
 import SliderTwo from '../SliderTwo/SliderTwo';
 
 const TopSell = () => {
-    return (
-        <div className='mx-[320px] mt-[60px]'>
-
-            <div data-aos="fade-down" data-aos-duration="2000" data-aos-easing="ease-in-out" className='section-title'>
-                <div className='section-line'></div>
-                <h1>Top Sell Products</h1>
-                <div className='section-line'></div>
-            </div>
-
-            <div data-aos="fade-left" data-aos-duration="1500" className='mt-[60px]'>
-                <SliderTwo />
-            </div>
-
-            <hr className='mt-[67px]' />
-
+  return (
+    <div className='mx-[320px] mt-[60px]'>
+      <div data-aos="fade-down" data-aos-duration="2000" data-aos-easing="ease-in-out" className='section-title'>
+        <div className='section-line'></div>
+        <div className='section-title-text'>
+          <h1>Top Sell Products</h1>
         </div>
-    );
+        <div className='section-line'></div>
+      </div>
+      <div data-aos="fade-left" data-aos-duration="1500" className='mt-[60px]'>
+        <SliderTwo />
+      </div>
+      <hr className='mt-[67px]' />
+    </div>
+  );
 };
 
 export default TopSell;
